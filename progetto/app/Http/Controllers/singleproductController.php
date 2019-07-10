da6b37f3823d1d5782a073e8eb8e916c40fe97c5
miripom <mirianapompilio@gmailcom>
2019-07-10 11:07:30 +0200

    bla

--- progetto/app/Http/Controllers/singleproductController.php
+++ progetto/app/Http/Controllers/singleproductController.php
@@ -13,14 +13,12 @@ class singleproductController extends Controller
     {
         $details= DB::table('product')
             ->join('gallery', 'product.id', '=', 'gallery.product_id')
-            //->join('category', 'category.id', '=', 'product.category_id')
             ->select('product.name', 'gallery.path' , 'product.id', 'product.description', 'product.price','product.brand')
             ->where('product.id', "=", $id)
-           // ->where('category.type', "=", $sex)
             ->groupby('product.id', 'gallery.product_id')
             ->get() ;
 
-        //echo $details;
+
         return view('/single-product-details', compact('details'));
     }
 
@@ -31,7 +29,7 @@ class singleproductController extends Controller
             ->join('gallery','product.id','=','gallery.product_id')
             ->select('product.name', 'gallery.path' , 'product.id', 'product.price','product.brand')
         ->where('product.name','like',"%{$query}%",'OR','product.brand','like',"%{$query}%" )
-        ->where('product.price','like',"%{$query}%",'OR','product.brand','like',"%{$query}%" )
+        ->where('category.name','like',"%{$query}%",'OR','product.brand','like',"%{$query}%" )
         ->where('product.brand','like',"%{$query}%",'OR','product.brand','like',"%{$query}%" )->get();
 
 
